b4c78e133678a396fb831861470aeba988f2c8b7
MFaisalZaki <mustafa.faisal.eng@gmail.com>
2026-05-31 12:57:40 +0100

    regular push.

--- behaviour_diversity_counter/features/resources.py
+++ behaviour_diversity_counter/features/resources.py
@@ -20,6 +20,26 @@ class ResourceCountSimulator(DimensionConstructorSimulator):
         for action in plan.actions:
             for used_resource in set.intersection(set(map(str, action.actual_parameters)), set(self.addinfo['objects'])):
                 resource_usage[used_resource] += 1
+        return ' $$ '.join(map(lambda e: f'{e[0]}={e[1]}', resource_usage.items()))
+    
+    def distance(self, plan1, plan2):
+        assert False, 'Distance function is not implemented for ResourceUsedSimulator.'
+    
+
+class ResourceUsedSimulator(DimensionConstructorSimulator):
+    def __init__(self, task, addinfo):
+        super().__init__(task, 'ru', {'resources_list': parse_resource_file(addinfo)})
+        self.addinfo['objects'] = set(map(str,filter(lambda e: e.name in set(map(lambda e: e['name'], self.addinfo['resources_list'].values())), self.task.all_objects)))
+    
+    def _estimate_domain(self):
+        # the maximum resource count is the number of objects that can be used as resources.
+        self.estimated_domain_size = len(set(chain.from_iterable(combinations(self.addinfo['objects'], r) for r in range(1, len(self.addinfo['objects']) + 1))))
+
+    def plan_behaviour(self, plan):
+        resource_usage = {o: 0 for o in self.addinfo['objects']}
+        for action in plan.actions:
+            for used_resource in set.intersection(set(map(str, action.actual_parameters)), set(self.addinfo['objects'])):
+                resource_usage[used_resource] += 1
         used = frozenset(o for o, c in resource_usage.items() if c > 0)
         self.domain.add(used)
         # encode the *set* of used resources so distance() can compute Jaccard.
@@ -36,25 +56,6 @@ class ResourceCountSimulator(DimensionConstructorSimulator):
         if not s1 and not s2:
             return 0.0
         return 1.0 - len(s1 & s2) / len(s1 | s2)
-
-class ResourceUsedSimulator(DimensionConstructorSimulator):
-    def __init__(self, task, addinfo):
-        super().__init__(task, 'ru', {'resources_list': parse_resource_file(addinfo)})
-        self.addinfo['objects'] = set(map(str,filter(lambda e: e.name in set(map(lambda e: e['name'], self.addinfo['resources_list'].values())), self.task.all_objects)))
-    
-    def _estimate_domain(self):
-        # the maximum resource count is the number of objects that can be used as resources.
-        self.estimated_domain_size = len(set(chain.from_iterable(combinations(self.addinfo['objects'], r) for r in range(1, len(self.addinfo['objects']) + 1))))
-
-    def plan_behaviour(self, plan):
-        resource_usage = {o: 0 for o in self.addinfo['objects']}
-        for action in plan.actions:
-            for used_resource in set.intersection(set(map(str, action.actual_parameters)), set(self.addinfo['objects'])):
-                resource_usage[used_resource] += 1
-        return ' $$ '.join(map(lambda e: f'{e[0]}={e[1]}', resource_usage.items()))
-    
-    def distance(self, plan1, plan2):
-        assert False, 'Distance function is not implemented for ResourceUsedSimulator.'
 
 class ResourceTransformer(Transformer):
     def resource_line(self, token):
